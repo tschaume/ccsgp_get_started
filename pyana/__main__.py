076314304c6f5f722043428e49eb7b5fc9e3ba5f
Patrick Huck <phuck@lbl.gov>
2014-01-08 16:54:01 -0800

    add pyana init and main

---
+++ pyana/__main__.py
@@ -0,0 +1,15 @@
+import logging, argparse
+from examples.gp_from_txt import gp_from_txt
+
+# parse arguments
+parser = argparse.ArgumentParser()
+#parser.add_argument("inputDir", help="input file")
+parser.add_argument("--log", help="show log output", action="store_true")
+args = parser.parse_args()
+loglevel = 'DEBUG' if args.log else 'WARNING'
+logging.basicConfig(
+  format='%(message)s', level=getattr(logging, loglevel)
+)
+
+# run analysis
+logging.debug( gp_from_txt() )
