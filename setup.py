9a059893aa749911ca902382ae25608d2bec2a7b
Patrick Huck <phuck@lbl.gov>
2014-01-13 11:35:29 -0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from ez_setup import use_setuptools
 use_setuptools()
-
 from setuptools import setup, find_packages
+
 setup(
   name = 'ccsgp_get_started',
   version = '1.0',
@@ -12,6 +12,6 @@ setup(
   url = 'https://github.com/tschaume/ccsgp_get_started',
   packages = find_packages(),
   install_requires = [
-    'wsgiref==0.1.2', 'numpy==1.8.0', 'gnuplot-py==1.8'#, 'h5py==2.2.1'
+     'h5py==2.2.1', 'gnuplot-py==1.8', 'numpy==1.8.0', 'wsgiref==0.1.2'
   ]
 )
