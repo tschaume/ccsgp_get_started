a9529e888b5f7430baac5a470c8f6d201597f0c8
Patrick Huck <phuck@lbl.gov>
2014-01-19 19:47:40 -0800

    get started on make_panel test

---
+++ pyana/examples/gp_panel.py
@@ -0,0 +1,30 @@
+import logging, argparse, os, sys, re
+import numpy as np
+from collections import OrderedDict
+from .utils import getWorkDirs, checkSymLink
+from ..ccsgp.ccsgp import make_panel
+
+def gp_panel(version):
+  """example for a panel plot using QM12 data (see gp_xfac)"""
+  inDir, outDir = getWorkDirs()
+  inDir = os.path.join(inDir, version)
+  input = {}
+  for file in os.listdir(inDir):
+    energy = re.compile('\d+').search(file).group()
+    key = ' '.join([energy, 'GeV'])
+    file_url = os.path.join(inDir, file)
+    input[key] = np.loadtxt(open(file_url, 'rb')).reshape((-1,5))
+  print input
+  return 'done'
+
+if __name__ == '__main__':
+  checkSymLink()
+  parser = argparse.ArgumentParser()
+  parser.add_argument("version", help="version = subdir name of input dir")
+  parser.add_argument("--log", help="show log output", action="store_true")
+  args = parser.parse_args()
+  loglevel = 'DEBUG' if args.log else 'WARNING'
+  logging.basicConfig(
+    format='%(message)s', level=getattr(logging, loglevel)
+  )
+  print gp_panel(args.version)
