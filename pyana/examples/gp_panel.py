2aa0595b26624de2e1c36c00edcfa747b5e68111
Patrick Huck <phuck@lbl.gov>
2014-01-19 22:19:32 -0800

    gp_panel works but buggy

--- pyana/examples/gp_panel.py
+++ pyana/examples/gp_panel.py
@@ -3,18 +3,28 @@ import numpy as np
 from collections import OrderedDict
 from .utils import getWorkDirs, checkSymLink
 from ..ccsgp.ccsgp import make_panel
+from ..ccsgp.utils import getOpts
 
 def gp_panel(version):
   """example for a panel plot using QM12 data (see gp_xfac)"""
   inDir, outDir = getWorkDirs()
   inDir = os.path.join(inDir, version)
-  input = {}
+  data = {}
   for file in os.listdir(inDir):
     energy = re.compile('\d+').search(file).group()
     key = ' '.join([energy, 'GeV'])
     file_url = os.path.join(inDir, file)
-    input[key] = np.loadtxt(open(file_url, 'rb')).reshape((-1,5))
-  print input
+    data[key] = np.loadtxt(open(file_url, 'rb')).reshape((-1,5))
+  make_panel(
+    dpt_dict = OrderedDict(
+      (k, [ [v], [getOpts(0)], ['data'] ])
+      for k, v in data.iteritems()
+    ),
+    name = os.path.join(outDir, 'panel%s' % version),
+    ylabel = 'invariant yield',
+    xlabel = 'invariant mass (GeV/c^{2})',
+    ylog = True, xr = [0, 1.2]
+  )
   return 'done'
 
 if __name__ == '__main__':
