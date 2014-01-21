c1ccdd939aa391a90b6567dc4dcf11beafaf8421
Patrick Huck <phuck@lbl.gov>
2014-01-21 06:02:40 -0800

    update install docs

--- pyana/__init__.py
+++ pyana/__init__.py
@@ -26,34 +26,10 @@ Installation
    `hdf5` is optional but if you'd like to save your image data to HDF5, you'll
    have to install it.
 
-2. clone the `ccsgp_get_started` git repository.
-
-   Include the submodule with the test data by cloning the git repository
-   recursively::
+2. clone the `ccsgp_get_started` git repository::
 
      $ git clone https://github.com/tschaume/ccsgp_get_started.git --recursive
      $ cd ccsgp_get_started/
-
-   Pull in public STAR dielectron data into a new branch to use gp_xfac and gp_panel::
-
-     $ git remote add dielec_public http://gitlab.the-huck.com/star/dielectron_data_public.git
-     $ git checkout -b star_dielec
-     $ git pull dielec_public master
-
-   If you are part of the STAR collaboration you can also pull in the protected
-   STAR dielectron data to include in gp_panel::
-
-     $ git remote add dielec_protect http://cgit.the-huck.com/dielectron_data_protected
-     $ git pull dielec_protect master
-       (enter STAR protected credentials)
-
-   Or alternatively, for source code only, clone and init the `ccsgp`
-   submodule separately::
-
-     $ git clone https://github.com/tschaume/ccsgp_get_started.git
-     $ cd ccsgp_get_started/
-     $ git submodule init pyana/ccsgp
-     $ git submodule update
 
 3. init the virtualenv_, activate it and install all requirements::
 
@@ -70,15 +46,35 @@ Installation
    use it, uncomment the h5py requirement in ``requirements.txt`` and rerun ``$
    pip install -r requirements.txt``.
 
-4. If you intend to run the examples (and you cloned recursively above), symlink
-   *pyanaDir* to *ccsgp_get_started_data*::
+4. If you intend to run the examples, clone the test data repository (somewhere
+   outside of ccsgp_get_started repository)::
 
-     $ ln -s ccsgp_get_started_data pyanaDir
+     $ git clone http://gitlab.the-huck.com/github/ccsgp_get_started_data.git
+
+   and symlink *pyanaDir* to the *ccsgp_get_started_data* directory::
+
+     $ cd ccsgp_get_started/
+     $ ln -s <path/to/ccsgp_get_started_data> pyanaDir
 
    `pyana.aux.utils.checkSymLink` checks for the *pyanaDir* symbolic link and
    the code won't run without it. Hence you need to generate a symlink called
    *pyanaDir* either to *ccsgp_get_started_data* or your own input/output
    directory (preferably separated from the code repository).
+
+   Pull in public STAR dielectron data into a new branch to use gp_xfac and gp_panel::
+
+     $ cd <path/to/ccsgp_get_started_data>
+     $ git remote add dielec_public http://gitlab.the-huck.com/star/dielectron_data_public.git
+     $ git checkout -b star_dielec
+     $ git pull dielec_public master
+
+   If you are part of the STAR collaboration you can also pull in the protected
+   STAR dielectron data to include it in gp_panel::
+
+     $ git remote add dielec_protect http://cgit.the-huck.com/dielectron_data_protected
+     $ git pull dielec_protect master
+       (enter STAR protected credentials)
+
 
 .. _package: https://github.com/tschaume/ccsgp_get_started
 .. _ccsgp: https://github.com/tschaume/ccsgp
