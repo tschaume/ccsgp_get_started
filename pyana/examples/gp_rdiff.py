4432ebbf2fc1abbc7c97f0cfb6a3b662e7606f0c
Patrick Huck <phuck@lbl.gov>
2014-02-07 04:55:56 -0500

    gp_rdiff: 19GeV non-coinciding correction works

--- pyana/examples/gp_rdiff.py
+++ pyana/examples/gp_rdiff.py
@@ -8,14 +8,33 @@ from ..ccsgp.utils import getOpts, zip_flat
 from ..ccsgp.config import default_colors
 from uncertainties import unumpy as unp
 from uncertainties.umath import fsum
+from decimal import Decimal
 
 xshift = 0.01
 yunit = 1.0e-3
+
+def getUArray(npArr):
+  """uncertainty array multiplied by binwidth (col2 = dx)"""
+  return unp.uarray(npArr[:,1], npArr[:,4]) * npArr[:,2] * 2
+
+def getEdges(npArr):
+  """get np array of bin edges"""
+  edges = np.concatenate(([0], npArr[:,0] + npArr[:,2]))
+  return np.array([Decimal(str(i)) for i in edges])
+
+def getMaskIndices(mask):
+  """get lower and upper index of mask"""
+  return [
+    list(mask).index(True), len(mask) - 1 - list(mask)[::-1].index(True)
+  ]
 
 def gp_rdiff(version):
   """example for ratio or difference plots using QM12 data (see gp_panel)
 
   - uses uncertainties package for easier error propagation and rebinning
+  - stat. error for medium = 0!
+  - stat. error for cocktail ~ 0!
+  - statistical error bar on data stays the same for diff
   - TODO: check whether cocktail & data/medium edges coincide!
   - TODO: implement ratio!
   - TODO: adjust statistical error on data for ratio!
@@ -33,37 +52,53 @@ def gp_rdiff(version):
     data_type = re.sub('%s\.dat' % energy, '', file)
     file_url = os.path.join(inDir, file)
     data_import = np.loadtxt(open(file_url, 'rb'))
-    data_import = data_import[data_import[:,0] < 1.5]
+    data_import = data_import[data_import[:,0] < 0.8]
     if data_type == 'data': data[energy] = data_import
     elif data_type == 'cocktail': cocktail[energy] = data_import
     else: medium[energy] = data_import
+
   dataOrdered = OrderedDict()
   for energy in sorted(data, key=int):
-    # data uncertainty array multiplied by binwidth (col2 = dx)
-    uData = unp.uarray(data[energy][:,1], data[energy][:,4])
-    uData *= data[energy][:,2] * 2
-    if energy in medium:
-      # medium uncertainty array multiplied by binwidth
-      # stat. error for medium = 0!
-      uMedium = unp.uarray(medium[energy][:,1], medium[energy][:,4])
-      uMedium *= medium[energy][:,2] * 2
-    # cocktail uncertainty array multiplied by binwidth
-    # stat. error for cocktail ~ 0!
-    uCocktail = unp.uarray(cocktail[energy][:,1], cocktail[energy][:,4])
-    uCocktail *= cocktail[energy][:,2] * 2
-    # data bin edges and cocktail bin centers
-    edges = np.concatenate(([0], data[energy][:,0] + data[energy][:,2]))
-    xc = cocktail[energy][:,0]
+    # data & bin edges
+    uData = getUArray(data[energy])
+    eData = getEdges(data[energy])
+    uCocktail = getUArray(cocktail[energy])
+    eCocktail = getEdges(cocktail[energy])
+    uMedium = getUArray(medium[energy]) if energy in medium else None
     # loop data bins
-    for i, (e0, e1) in enumerate(zip(edges[:-1], edges[1:])):
+    for i, (e0, e1) in enumerate(zip(eData[:-1], eData[1:])):
+      # get mask and according indices
+      mask = (eCocktail >= e0) & (eCocktail <= e1)
+      idx = getMaskIndices(mask)
+      # determine coinciding flags
+      eCl, eCu = eCocktail[idx[0]], eCocktail[idx[1]]
+      not_coinc_low, not_coinc_upp = (eCl != e0), (eCu != e1)
+      # get cocktail sum in data bin (always w/o last bin)
+      uCocktailSum = fsum(uCocktail[mask[:-1]][:-1])
+      print ('%s> %g - %g: {}' % (energy, e0, e1)).format(uCocktailSum)
+      # get correction for non-coinciding edges
+      if not_coinc_low:
+        eCl_bw = eCl - eCocktail[idx[0]-1]
+        corr_low = (eCl - e0) / eCl_bw
+        abs_corr_low = float(corr_low) * uCocktail[idx[0]-1]
+        print ('    low: %g == %g -> %g (%g) -> %g -> {}' % (
+          e0, eCl, eCl - e0, eCl_bw, corr_low
+        )).format(abs_corr_low)
+      if not_coinc_upp:
+        eCu_bw = eCocktail[idx[1]+1] - eCu
+        corr_upp = (e1 - eCu) / eCu_bw
+        abs_corr_upp = float(corr_upp) * uCocktail[idx[1]]
+        print ('    upp: %g == %g -> %g (%g) -> %g -> {}' % (
+          e1, eCu, e1 - eCu, eCu_bw, corr_upp
+        )).format(abs_corr_upp)
+      continue
       # calc. difference and divide by data binwidth again
-      uDiff = uData[i] - fsum(uCocktail[(xc > e0) & (xc < e1)])
+      uDiff = uData[i] - (uCocktailSum)
       uDiff /= data[energy][i,2] * 2 * yunit
       # set data point
       xs = xshift if energy == '39' else 0.
       dp = [
         data[energy][i,0] + xs, uDiff.nominal_value,
-        # statistical error bar on data stays the same for diff
         0., data[energy][i,3] / yunit, uDiff.std_dev
       ]
       # build list of data points
