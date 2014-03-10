e06385041cb4839e18b83eb5f0aaff0cb3583505
Patrick Huck <phuck@lbl.gov>
2014-03-09 22:15:37 -0700

    gp_rapp: plot multiple ranges

--- pyana/examples/gp_rapp.py
+++ pyana/examples/gp_rapp.py
@@ -6,33 +6,43 @@ from .utils import getWorkDirs, checkSymLink
 from .gp_rdiff import getUArray, getEdges, getCocktailSum, enumzipEdges
 from decimal import Decimal
 
-ranges = [0.3, 0.7] # TODO: same for IMR
+ranges = [ 0, 0.3, 0.7, 1.1, 3. ]
+titles = [ 'pi0', 'LMR', 'omphi', 'IMR' ]
 
 def gp_rapp():
   """rho in-medium ratios by Rapp (based on protected data)"""
   inDir, outDir = getWorkDirs()
-  eRanges = np.array([Decimal(str(e)) for e in ranges])
+  eRanges = np.array([ Decimal(str(e)) for e in ranges ])
   # prepare data
-  yields = []
+  yields = {}
   for infile in os.listdir(inDir):
     energy = re.compile('\d+').search(infile).group()
     medium = np.loadtxt(open(os.path.join(inDir, infile), 'rb'))
     uMedium = getUArray(medium)
     eMedium = getEdges(medium)
     for i, (e0, e1) in enumzipEdges(eRanges):
+      print i, e0, e1
       uSum = getCocktailSum(e0, e1, eMedium, uMedium)
       logging.debug('%s> %g - %g: %r' % (energy, e0, e1, uSum))
-      yields.append([float(energy), uSum.nominal_value, 0, 0, 0])
-  data = np.array(sorted(yields))
-  data[:,1] /= data[-1,1] # divide by 200
+      dp = [float(energy), uSum.nominal_value, 0, 0, 0]
+      if titles[i] in yields: yields[titles[i]].append(dp)
+      else: yields[titles[i]] = [ dp ]
+  data = dict( # sort by energy
+    (k, np.array(sorted(v)))
+    for k, v in yields.iteritems()
+  )
+  for k in data: data[k][:,1] /= data[k][-1,1] # divide by 200
   # make plot
+  nSets = len(data)
   make_plot(
-    data = [ data[:-1] ],
-    properties = [ 'lt 1 lw 4 ps 1.5 lc %s pt 18' % default_colors[0] ],
-    titles = [ 'LMR (0.3 - 0.7 GeV/c^{2})' ],
-    name = os.path.join(outDir, 'rapp'),
+    data = [ data[k][:-1] for k in data ],
+    properties = [
+      'with linespoints lt 1 lw 4 ps 1.5 lc %s pt 18' % default_colors[i]
+      for i in xrange(nSets)
+    ],
+    titles = titles, name = os.path.join(outDir, 'rapp'),
     xlabel = '{/Symbol \326}s_{NN} (GeV)', ylabel = 'Rapp Ratio to 200 GeV',
-    lmargin = 0.1, key = ['width -3', 'left']
+    lmargin = 0.1, key = ['left'], yr = [0.1, 0.8]
   )
   return 'done'
 
