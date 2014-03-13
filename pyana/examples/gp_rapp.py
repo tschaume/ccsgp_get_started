d53e55b9f3ce0ca1cc6baad1edfaf1e61a5012d2
Patrick Huck <phuck@lbl.gov>
2014-03-13 13:16:58 -0700

    utils,gp_rdiff,gp_rapp: getMassRangesSums

--- pyana/examples/gp_rapp.py
+++ pyana/examples/gp_rapp.py
@@ -2,31 +2,17 @@ import logging, argparse, os, re
 import numpy as np
 from ..ccsgp.config import default_colors
 from ..ccsgp.ccsgp import make_plot
-from .utils import getWorkDirs, checkSymLink
-from .gp_rdiff import getUArray, getEdges, getCocktailSum, enumzipEdges
-from decimal import Decimal
-
-ranges = [ 0, 0.3, 0.7, 1.1, 3. ]
-titles = [ 'pi0', 'LMR', 'omphi', 'IMR' ]
+from .utils import getWorkDirs, checkSymLink, getMassRangesSums
 
 def gp_rapp():
   """rho in-medium ratios by Rapp (based on protected data)"""
   inDir, outDir = getWorkDirs()
-  eRanges = np.array([ Decimal(str(e)) for e in ranges ])
   # prepare data
   yields = {}
   for infile in os.listdir(inDir):
     energy = re.compile('\d+').search(infile).group()
     medium = np.loadtxt(open(os.path.join(inDir, infile), 'rb'))
-    uMedium = getUArray(medium)
-    eMedium = getEdges(medium)
-    for i, (e0, e1) in enumzipEdges(eRanges):
-      print i, e0, e1
-      uSum = getCocktailSum(e0, e1, eMedium, uMedium)
-      logging.debug('%s> %g - %g: %r' % (energy, e0, e1, uSum))
-      dp = [float(energy), uSum.nominal_value, 0, 0, 0]
-      if titles[i] in yields: yields[titles[i]].append(dp)
-      else: yields[titles[i]] = [ dp ]
+    getMassRangesSums(energy, medium, yields)
   data = dict( # sort by energy
     (k, np.array(sorted(v)))
     for k, v in yields.iteritems()
@@ -40,7 +26,8 @@ def gp_rapp():
       'with linespoints lt 1 lw 4 ps 1.5 lc %s pt 18' % default_colors[i]
       for i in xrange(nSets)
     ],
-    titles = titles, name = os.path.join(outDir, 'rapp'),
+    titles = data.keys(), # TODO: titles order correct?
+    name = os.path.join(outDir, 'rapp'),
     xlabel = '{/Symbol \326}s_{NN} (GeV)', ylabel = 'Rapp Ratio to 200 GeV',
     lmargin = 0.1, key = ['left'], yr = [0.1, 0.8]
   )
--- pyana/examples/utils.py
+++ pyana/examples/utils.py
@@ -1,4 +1,10 @@
 import sys, os, itertools, inspect, logging
+import numpy as np
+from uncertainties import unumpy as unp
+from uncertainties.umath import fsum
+from decimal import Decimal
+mass_titles = [ 'pi0', 'LMR', 'omphi', 'IMR' ]
+eRanges = np.array([ Decimal(str(e)) for e in [ 0, 0.3, 0.7, 1.1, 3. ] ])
 
 def checkSymLink():
   """check for symbolic link to input directory"""
@@ -26,3 +32,80 @@ def getWorkDirs():
     logging.critical('create input dir %s to continue!' % inDir)
     sys.exit(1)
   return inDir, outDir
+
+def getUArray(npArr):
+  """uncertainty array multiplied by binwidth (col2 = dx)"""
+  return unp.uarray(npArr[:,1], npArr[:,4]) * npArr[:,2] * 2
+
+def getEdges(npArr):
+  """get np array of bin edges"""
+  edges = np.concatenate(([0], npArr[:,0] + npArr[:,2]))
+  return np.array([Decimal(str(i)) for i in edges])
+
+def getMaskIndices(mask):
+  """get lower and upper index of mask"""
+  return [
+    list(mask).index(True), len(mask) - 1 - list(mask)[::-1].index(True)
+  ]
+
+def enumzipEdges(eArr):
+  """zip and enumerate edges into pairs of lower and upper limits"""
+  return enumerate(zip(eArr[:-1], eArr[1:]))
+
+def getCocktailSum(e0, e1, eCocktail, uCocktail):
+  """get the cocktail sum for a given data bin range"""
+  # get mask and according indices
+  mask = (eCocktail >= e0) & (eCocktail <= e1)
+  # data bin range wider than single cocktail bin
+  if np.any(mask):
+    idx = getMaskIndices(mask)
+    # determine coinciding flags
+    eCl, eCu = eCocktail[idx[0]], eCocktail[idx[1]]
+    not_coinc_low, not_coinc_upp = (eCl != e0), (eCu != e1)
+    # get cocktail sum in data bin (always w/o last bin)
+    uCocktailSum = fsum(uCocktail[mask[:-1]][:-1])
+    logging.debug('    sum: {}'.format(uCocktailSum))
+    # get correction for non-coinciding edges
+    if not_coinc_low:
+      eCl_bw = eCl - eCocktail[idx[0]-1]
+      corr_low = (eCl - e0) / eCl_bw
+      abs_corr_low = float(corr_low) * uCocktail[idx[0]-1]
+      uCocktailSum += abs_corr_low
+      logging.debug(('    low: %g == %g -> %g (%g) -> %g -> {} -> {}' % (
+        e0, eCl, eCl - e0, eCl_bw, corr_low
+      )).format(abs_corr_low, uCocktailSum))
+    if not_coinc_upp:
+      if idx[1]+1 < len(eCocktail):
+        eCu_bw = eCocktail[idx[1]+1] - eCu
+        corr_upp = (e1 - eCu) / eCu_bw
+        abs_corr_upp = float(corr_upp) * uCocktail[idx[1]]
+      else:# catch last index (quick fix!)
+        abs_corr_upp = eCu_bw = corr_upp = 0
+      uCocktailSum += abs_corr_upp
+      logging.debug(('    upp: %g == %g -> %g (%g) -> %g -> {} -> {}' % (
+        e1, eCu, e1 - eCu, eCu_bw, corr_upp
+      )).format(abs_corr_upp, uCocktailSum))
+  else:
+    mask = (eCocktail >= e0)
+    idx = getMaskIndices(mask) # only use first index
+    # catch if already at last index
+    if idx[0] == idx[1] and idx[0] == len(eCocktail)-1:
+      corr = (e1 - e0) / (eCocktail[idx[0]] - eCocktail[idx[0]-1])
+      uCocktailSum = float(corr) * uCocktail[idx[0]-1]
+    else: # default case
+      corr = (e1 - e0) / (eCocktail[idx[0]+1] - eCocktail[idx[0]])
+      uCocktailSum = float(corr) * uCocktail[idx[0]]
+    logging.debug('    sum: {}'.format(uCocktailSum))
+  return uCocktailSum
+
+def getMassRangesSums(energy, indata, outdata):
+  uMedium = getUArray(indata)
+  eMedium = getEdges(indata)
+  for i, (e0, e1) in enumzipEdges(eRanges):
+    uSum = getCocktailSum(e0, e1, eMedium, uMedium)
+    logging.debug('%s> %g - %g: %r' % (energy, e0, e1, uSum))
+    dp = [
+      float(energy), uSum.nominal_value, 0, uSum.std_dev, 0
+    ] # TODO: syst.  uncertainties
+    if mass_titles[i] in outdata: outdata[mass_titles[i]].append(dp)
+    else: outdata[mass_titles[i]] = [ dp ]
