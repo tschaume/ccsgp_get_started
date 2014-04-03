649b77b41a611a9b4bec402413332004fb2d2119
Patrick Huck <phuck@lbl.gov>
2014-04-03 15:45:22 -0700

    add gp_lcltpt for linetype/color/pointtype

---
+++ pyana/examples/gp_lcltpt.py
@@ -0,0 +1,30 @@
+import os
+import numpy as np
+from ..ccsgp.ccsgp import make_plot
+from .utils import getWorkDirs, checkSymLink
+from ..ccsgp.config import default_colors
+
+def gp_lcltpt():
+  inDir, outDir = getWorkDirs()
+  nSets = len(default_colors)
+  make_plot(
+    data = [
+      np.array([ [0,i,0,0,0], [1,i,0,0,0] ])
+      for i in xrange(nSets)
+    ],
+    properties = [
+      'with linespoints lw 4 lc %s lt %d pt %d' % (col, i, i)
+      for i, col in enumerate(default_colors)
+    ],
+    titles = [''] * nSets, yr = [-1, 51],
+    name = os.path.join(outDir, 'gp_lcltpt'),
+    ylabel = 'linecolor / linetype / pointtype', xlabel = '',
+  )
+  return 'done'
+
+if __name__ == '__main__':
+  import argparse
+  checkSymLink()
+  parser = argparse.ArgumentParser()
+  args = parser.parse_args()
+  print gp_lcltpt()
