aecc3b38f3e3cd9a094c85ad686db8ef76f85dc9
Patrick Huck <patrick@the-huck.com>
2015-01-02 19:35:46 -0800

    gp_effcorr: finish

--- pyana/examples/gp_effcorr.py
+++ pyana/examples/gp_effcorr.py
@@ -6,20 +6,48 @@ from ..ccsgp.config import default_colors
 import numpy as np
 
 def gp_syserr():
+    energies = ['19.6', '27', '39', '62.4']
     inDir, outDir = getWorkDirs()
     inDir = os.path.join(inDir, 'syserr')
-    data = {}
+    data = OrderedDict()
+    shift, factor = 0.7, -1.5
+    ymin = OrderedDict((energy, 100) for energy in energies)
+    ymax = OrderedDict((energy, -100) for energy in energies)
     for particle in ['e', 'pi']:
         for charge in ['plus', 'minus']:
             fname = particle + charge + '.dat'
-            data[fname] = np.loadtxt(open(os.path.join(inDir, fname), 'rb'))
+            key = '{/Symbol \160}' if particle == 'pi' else 'e'
+            key += '^{+}' if charge == 'plus' else '^{-}'
+            data[key] = np.loadtxt(open(os.path.join(inDir, fname), 'rb'))
+            # get y-values for error box
+            for dp in data[key]:
+                energy = '%g' % dp[0]
+                curymin, curymax = dp[1]-dp[3], dp[1]+dp[3]
+                if curymin < ymin[energy]: ymin[energy] = curymin
+                if curymax > ymax[energy]: ymax[energy] = curymax
+            # shift data for visibility
+            data[key][:,0] += factor*shift
+            factor += 1
+    print ymax
     make_plot(
         data = data.values(),
-        properties = [ 'lc %s lw 4 pt 18' % default_colors[i] for i in xrange(len(data)) ],
+        properties = [
+            'lc %s lw 5 ps 2 pt %d' % (default_colors[10+i/2],18+i%2)
+            for i in xrange(len(data))
+        ],
         titles = data.keys(),
-        xlabel = 'sqrt(s)', ylabel = 'syserr (%)',
+        tmargin = 0.98, rmargin = 0.99, bmargin = 0.13,
+        yr = [0,22], xr = [15,68],
+        xlabel = '{/Symbol \326}s_{NN} (GeV)',
+        ylabel = 'Relative Systematic Uncertainty on {/Symbol \145}_{qual}{/Symbol \327}{/Symbol \145}_{glDCA} (%)',
         name = os.path.join(outDir, 'syserr'),
-        size = '10in,8in',
+        size = '11in,8.5in', key = ['nobox', 'at graph 0.7,0.2', 'maxrows 2'],
+        lines = dict(('y=%s' % energy,'lw 4 lt 2 lc "black"') for energy in energies),
+        gpcalls = [
+            'object %d rectangle back from %f,%f to %f,%f fc rgb "#FF9999" lw 2 fs border lc rgb "#FF6666"' % (
+                50+i, float(energy)-1.5*shift, ymin[energy], float(energy)+1.5*shift, ymax[energy]
+            ) for i,energy in enumerate(energies)
+        ]
     )
 
 if __name__ == '__main__':
