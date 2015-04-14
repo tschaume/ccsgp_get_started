e87bea27c5df28c17d252e43dd6bc15b69aa030b
Patrick Huck <patrick@the-huck.com>
2015-04-14 16:03:45 -0700

    gp_purity: first complete version

--- pyana/examples/gp_purity.py
+++ pyana/examples/gp_purity.py
@@ -12,10 +12,15 @@ def gp_purity():
     contams = ['{/Symbol \160}', 'K', 'p', '{/Symbol \160}{/Symbol \160}']
     dtypes = ['purity'] + ['contam{}'.format(i+1) for i in range(len(contams))]
     data = OrderedDict()
+    sampfr = {0.4: [], 0.7: [], 1: []}
     for eidx,energy in enumerate(energies):
         ekey = ' '.join([getEnergy4Key(str(energy)), 'GeV'])
         data[ekey] = [[], [], []]
         for pidx,particle in enumerate(particles):
+            infile = os.path.join(inDir, '{}_{}_sampfr.dat'.format(particle, energy))
+            data_import = np.loadtxt(open(infile,'r'))
+            for x in sampfr:
+                sampfr[x].append(sum(data_import[data_import[:,0]<x][:,1]))
             for didx,dtype in enumerate(dtypes):
                 infile = os.path.join(inDir, '{}_{}_{}.dat'.format(
                     particle, energy, dtype
@@ -33,12 +38,23 @@ def gp_purity():
                 if title == 'p^{-}': title = '@^{\261}p'
                 if title == 'p^{+}': title = 'p'
                 data[ekey][2].append(title)
+    sampfr_tot = {}
+    for x in sampfr:
+        sampfr_tot[x] = sum(sampfr[x])/len(sampfr[x])
     make_panel(
         dpt_dict = data, name = os.path.join(outDir, 'purity'),
-        yr = [-0.03,1.03], xr = [0.15,1.85],
+        yr = [-0.03,1.03], xr = [0.12,1.87],
         xlabel = 'momentum, p (GeV/c)',
-        ylabel = 'fraction of candidate sample (%)',
-        layout = '2x2', size = '5in,7in', key = ['nobox', 'at graph 0.4,0.75'],
+        ylabel = 'fraction of candidate sample',
+        layout = '2x2', size = '5in,7in',
+        key = ['nobox', 'at graph 0.4,0.72'], lines = dict(
+            ('y={}'.format(x), 'lc {} lt 3 lw 3'.format(default_colors[-8]))
+            for x in sampfr
+        ), key_subplot_id = 2, gpcalls = [
+            'label %d "{/Helvetica=18 %.0f%%}" at %f,0.5 rotate center textcolor %s' % (
+                i+5, sampfr_tot[x]*100., x-0.04, default_colors[-8]
+            ) for i,x in enumerate(sampfr_tot)
+        ]
     )
 
 if __name__ == '__main__':
