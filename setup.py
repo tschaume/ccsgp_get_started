d39f9436044eb0ac34588559127149fc0e15bcb8
Patrick Huck <patrick@the-huck.com>
2015-05-21 21:17:55 -0700

    setup: try setup_requires

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
     dependency_links = [
         'http://sourceforge.net/projects/gnuplot-py/files/Gnuplot-py/1.8/gnuplot-py-1.8.tar.gz/download'
     ],
+    setup_requires = [ 'numpy==1.9.2' ],
     install_requires = [
         'numpy==1.9.2', 'Pint==0.5.1',
         'PyModelFit==0.1.2', 'uncertainties==2.4.4'
