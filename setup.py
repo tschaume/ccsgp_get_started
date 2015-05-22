b95f2e6671bb8b0fdc90493265e58212d0599aba
Patrick Huck <patrick@the-huck.com>
2015-05-21 18:35:17 -0700

    remove ez_setup, make dependency

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
   packages = find_packages(),
   install_requires = [
     #'h5py==2.2.1',
-    'pint==0.5.1', 'pymodelfit==0.1.2',
-    'uncertainties==2.4.4', 'gnuplot-py>=1.8', 'numpy>=1.8.0', 'wsgiref==0.1.2'
+    'ez-setup==0.9', 'Pint==0.5.1', 'PyModelFit==0.1.2',
+    'uncertainties==2.4.4', 'gnuplot-py==1.8', 'numpy==1.9.2', 'wsgiref==0.1.2'
   ]
 )
