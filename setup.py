d4e0b38f92f92f8dcf911e702a5e63c307a65587
Patrick Huck <patrick@the-huck.com>
2015-05-21 19:09:17 -0700

    remove ez_setup dependence

--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
-from ez_setup import use_setuptools
-use_setuptools()
-from setuptools import setup, find_packages
-
+from distutils.core import setup
 setup(
   name = 'ccsgp_get_started',
   version = '2.0',
@@ -12,7 +9,7 @@ setup(
   url = 'https://github.com/tschaume/ccsgp_get_started',
   download_url = 'https://github.com/tschaume/ccsgp_get_started/archive/v2.0.tar.gz',
   keywords = ['gnuplot', 'graph', 'plot', 'panel'],
-  packages = find_packages(),
+  packages = ['ccsgp_get_started'],
   install_requires = [
     #'h5py==2.2.1',
     'ez-setup==0.9', 'Pint==0.5.1', 'PyModelFit==0.1.2',
