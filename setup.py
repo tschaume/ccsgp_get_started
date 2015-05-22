a079dd6923f49e1b02409481e49c929066176f48
Patrick Huck <patrick@the-huck.com>
2015-05-21 19:11:53 -0700

    setup: remove install_requires

--- setup.py
+++ setup.py
@@ -10,9 +10,4 @@ setup(
   download_url = 'https://github.com/tschaume/ccsgp_get_started/archive/v2.0.tar.gz',
   keywords = ['gnuplot', 'graph', 'plot', 'panel'],
   packages = ['ccsgp_get_started'],
-  install_requires = [
-    #'h5py==2.2.1',
-    'ez-setup==0.9', 'Pint==0.5.1', 'PyModelFit==0.1.2',
-    'uncertainties==2.4.4', 'gnuplot-py==1.8', 'numpy==1.9.2', 'wsgiref==0.1.2'
-  ]
 )
