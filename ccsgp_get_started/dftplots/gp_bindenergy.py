a537d1b48e817c4e9d7a4925e6a2df0aaa034963
Johanna Huck <jobst@berkeley.edu>
2015-06-30 22:38:30 -0700

    gp_bindenergy: include experiments

--- ccsgp_get_started/dftplots/gp_bindenergy.py
+++ ccsgp_get_started/dftplots/gp_bindenergy.py
@@ -29,15 +29,15 @@ def gp_bindenergy(guest):
   :ivar file_url: absolute url to input file
   :ivar nSets: number of datasets
   :ivar nfiles: number of files
-  :ivar dx: bar width
-  :ivar gap: gap between 2 bars
+  :ivar dx: bar width estimated for specific amount of datasets
+  :ivar gap: space between 2 bars
   """
   # prepare color arrays for dataset
-  my_color_set = [default_colors[i] for i in range(0, 3)]
+  my_color_set = [default_colors[i] for i in range(0, 4)]
   my_color_array = []
   for color in my_color_set:
       my_color_array.append(colorscale(color[-7:-1], 0.8))
-      my_color_array.append(colorscale(color[-7:-1], 1.5))
+      my_color_array.append(colorscale(color[-7:-1], 1.4))
   # prepare input/output directories
   inDir, outDir = getWorkDirs()
   inDir = os.path.join(inDir, guest)
@@ -46,34 +46,37 @@ def gp_bindenergy(guest):
   # prepare data
   data = OrderedDict()
   files = os.listdir(inDir)
-  nfiles = len(files)
+  nfiles = len(files)-1
   dx = 0.7/nfiles
   gap = (1. - dx*nfiles)/2
   for idx,file in enumerate(files):
-    funct = os.path.splitext(file)[0]
-    if funct == 'experiments': continue
+    funct, ext = os.path.splitext(file)
+    if funct == 'experiments' or ext != '.dat': continue
     file_url = os.path.join(inDir, file)
     data[funct] = np.loadtxt(open(file_url, 'rb')) # load data
-    data[funct][:,0] += (idx - nfiles/2 + 0.5*(not nfiles%2)) * dx
+    data[funct][:,0] += (idx - (nfiles+1)/2. + 0.5*(not nfiles%2)) * dx
+  data['M06-L'] = data.pop('M06-L')
+  exp_data = np.loadtxt(open(os.path.join(inDir,'experiments.dat'), 'rb')) # load exp_data
   logging.debug(data) # shown if --log flag given on command line
   # generate plot using ccsgp.make_plot
   nSets = len(data)
   make_plot(
-    data = data.values(),
-    properties = [
+    data = [np.array([[1,-5]])] + data.values(),
+    properties = ['with boxes lt -1 lc %s' % default_colors[-10]] + [
         'with boxes lt -1 lc %s' % (my_color_array[i]) for i in range(nSets)
     ], gpcalls = [
         'boxwidth {} absolute'.format(dx),
         'style fill solid 1.0 border lt -1',
         'xtics ("Mg"1, "Mn"2, "Fe"3, "Co"4, "Ni"5, "Cu"6, "Zn"7)',
-        #'object 51 rectangle back fc rgb "grey" from {},0 to {},-60'.format(
-        #    1.5 + gap, 2.5 - gap
-        #),
-    ], titles = data.keys(), # use data keys as legend titles
+    ] + [
+        'arrow {} from {},{} to {},{} lw 4 lc {} nohead front'.format(
+            i+5, dp[0]-nfiles*dx/2., dp[1], dp[0]+nfiles*dx/2., dp[1], default_colors[-10]
+        ) for i,dp in enumerate(exp_data)
+    ], titles = ['experiments'] + data.keys(), # use data keys as legend titles
     name = os.path.join(outDir, guest), yreverse = True,
-    key = [ 'at graph 1., 1.2', 'maxrows 2', 'width -1.05' ],
+    key = [ 'at graph 1.07, 1.25', 'maxrows 3', 'width -1.1', 'nobox' ],
     ylabel = 'electronic binding energy (kJ/mol)',
-    xlabel = 'metals', rmargin = 0.99, tmargin = 0.85, size='8.5in,8in',
+    xlabel = 'metals', rmargin = 0.99, tmargin = 0.83, size='8.5in,8in',
     debug = True
   )
   return 'done'
